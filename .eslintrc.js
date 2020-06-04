47a16dcac09aa92aeb82ef7c9729d86fa9d15102
Sebastian Sulinski <info@sebastiansulinski.co.uk>
2020-06-04 18:00:39 +0100

    Compiled commonjs version

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,10 +2,13 @@ module.exports = {
   root: true,
   env: {
     node: true,
+    es6: true,
   },
   extends: ['eslint:recommended', 'plugin:prettier/recommended'],
   parserOptions: {
     parser: 'babel-eslint',
+    ecmaVersion: 2020,
+    sourceType: 'module',
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
